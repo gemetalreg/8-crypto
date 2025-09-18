acc4a81d02e4d0b6ac25e4461d5dc7abe8d497fe
Andrei <itunereg@gmail.com>
2025-09-18 08:17:22 +0300

    create crypto and check func

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+function crypto(password) {
+  const passArr = password.split("");
+  return passArr.reverse().join("");
+}
 
+function check(cryptoPassword, password) {
+  return cryptoPassword === crypto(password);
+}
 
+console.log(crypto("password"));
+console.log(check("drowssap", "password"));
+console.log(check("drowssap", "word"));
