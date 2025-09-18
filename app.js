6b49812a81e64a0db8d03886c0b63dcd623517bd
Andrei <itunereg@gmail.com>
2025-09-18 08:45:14 +0300

    second fix

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function crypto(password) {
   const passArr = password.split("");
-  return passArr
-    .slice(passArr.length / 2)
-    .reverse()
-    .concat(passArr.slice(0, passArr.length / 2).reverse())
-    .join("");
+  const part1 = passArr.slice(0, passArr.length / 2).reverse();
+  const part2 = passArr.slice(passArr.length / 2).reverse();
+  const temp = part2[part2.length / 2];
+  part2[part2.length / 2] = part2[part2.length / 2 - 1];
+  part2[part2.length / 2 - 1] = temp;
+  return part1.concat(part2).join("");
 }
 
 function check(cryptoPassword, password) {
@@ -12,5 +13,5 @@ function check(cryptoPassword, password) {
 }
 
 console.log(crypto("password"));
-console.log(check("drowssap", "password"));
-console.log(check("drowssap", "word"));
+console.log(check("ssapdorw", "password"));
+console.log(check("ssapdorw", "word"));
